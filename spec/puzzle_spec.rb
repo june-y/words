28d8b88a908bd12ed3bd17216929a3460c795c28
june-y <yolcuepa@gmail.com>
2018-10-07 22:11:47 -0400

    created the structure for testing

---
+++ spec/puzzle_spec.rb
@@ -0,0 +1,37 @@
+require "puzzle"
+
+=begin
+
+RULES:
+reject puzzle if its not a square -
+        pick a min size; min for words and puzzle size
+
+        word class?
+            can be read horizontally, vertically, diagonally, and both forwards and backwards
+            Words will be a minimum of two letters long
+
+
+INPUT:
+ first line of the text file will consist of the list of words to be found
+    separated by commas
+
+  remaining lines are randomized characters
+  and the number of rows will match the number of characters in a line. (what is this)
+
+
+
+OUTPUT:
+      WORD: (X,Y),(X,Y),(X,Y) (as many locations as letters)
+
+
+TESTING:
+    unit testing
+    integration testing
+
+=end
+
+describe Puzzle do
+# there should be a function to take input, which is a txt
+
+
+end
