cf235adb1f25a0e7b6aea909352512c6cc95adc5
june-y <yolcuepa@gmail.com>
2018-10-11 23:33:54 -0400

    added clean square function. I believe I originally started with isSquare, but I realized if the puzzle had empty line those were affecting the count, and I didn't have a true count. So I did clean square function first.

--- lib/puzzle.rb
+++ lib/puzzle.rb
@@ -7,21 +7,18 @@ require 'csv'
       prompt = '> '
       puts "Enter text will full path please"
       input_file = $stdin.gets.chomp
-      file_array = IO.readlines(input_file)
+      @file_array = IO.readlines(input_file)
     end
 
     def receive_input()
       prompt = '> '
       puts "Enter text will full path please"
       input_file = $stdin.gets.chomp
-      file_array = IO.readlines(input_file)
-      @@word_list = file_array[0].downcase.strip.split(/,/)
+      @file_array = IO.readlines(input_file)
+      @@word_list = @file_array[0].downcase.strip.split(/,/)
       @@letter_list = @@word_list.join("").chars.uniq
-      puzzle_row_count  = file_array.count
-      grid_size = (puzzle_row_count - 1)
-      @@square_with_letters = file_array[1..grid_size]
-      @@square_with_letters = @@square_with_letters
-      puts @@square_with_letters
+      cleanSquare()
+      grid_size = @@square_with_letters.count
       return @@word_list
     end
 
@@ -42,17 +39,37 @@ require 'csv'
 
 
 
+    def cleanSquare() #plan for this function to work
+        puzzle_row_count  = @file_array.count
+
+        replace_With = [["\t",""],[",",""],["\s+",""],["\n",""]]
+        @@square_with_letters = []
+        @file_array.drop(1).each do |line|
+          #read line and remove tabs,commas, empty readlines
+          #for each letter, enter x-axis,y-axis, isin_Words value, and
+          #figure out a function that per word, what is the next letter in line,
+          #and what is that position
+          replace_With.each {|replace_With| line.gsub!(replace_With[0], replace_With[1])}
+          @@square_with_letters << line unless line.chomp.empty?
+          end
+          ##  grid_size = @@square_with_letters.count #count of the arrays after cleaning
+          return @@square_with_letters
+      end
+
+
+
+
    def isWord_bigger_than_puzzle()
      puts @@word_list.max_by(&:length)
-     puts @@square_with_letters.reject { |c| c.empty? }
+
    end #isWord_bigger_than_puzzle
 
 
 end #puzzle class
 
-test2 = Puzzle.new
-test2.receive_input
-test2.isWord_bigger_than_puzzle()
+#test2 = Puzzle.new
+#test2.receive_input
+#test2.isWord_bigger_than_puzzle()
 #test2.letters_in_wordlist
 #test2.isin_Words('a')
 
@@ -62,6 +79,14 @@ These are notes
 #will need a function for grid_size, location hash
 ## min size for puzzle the longest word has to be able to fit on one line
 ## min size for puzzle is longest word <= column/row count
+
+
+#the puzzle solver class will read like:
+# search_horizontally = add to x one at a time, while running function for nearby words
+# search_Vertically = add to y axis  ....
+# search diagonally_descending = starting at letter, add to y, then add to x
+# search diagonally_ascending = starting at letter, add to x, then y (kind of assumes your at the bottom)
+# search backwards = all axes?? uncertain but will come back
 
 
 
--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -36,48 +36,49 @@ TESTING:
 describe Puzzle do
 
   describe ".receive_single_input" do #reading a txt file with a single line
-     context "given a text file" do
-       it "opens file and reads it" do
-         puzzle = Puzzle.new
-         #expect(puzzle.receive_single_input()).to be_an_instance_of(Array)
-         expect(puzzle.receive_single_input()).to eql(["hello\n"]) # test_files/fixtures/hello.txt
-       end
-     end
-   end
-
-  describe ".letters_in_wordlist" do #is this letter in the words in the word list (goes thru all)
-   context "given a list of words" do
-     it "will be one array with unique letters looking for" do
+   context "given a text file" do
+     it "opens file and reads it" do
        puzzle = Puzzle.new
-       puzzle.receive_input() #test_files/test2.txt
-       expect(puzzle.letters_in_wordlist()).to eq(["j", "u", "v", "e", "n", "i", "l", "c", "t", "a", "r", "g", "o", "d"])
-       #right now this is failing because input for this function is based off
-       #of the ARGV[0] from receive_input.
+       #expect(puzzle.receive_single_input()).to be_an_instance_of(Array)
+       expect(puzzle.receive_single_input()).to eql(["hello\n"]) # test_files/fixtures/hello.txt
      end
    end
  end
 
-  describe ".isin_Words" do #is this letter in the words in the word list (goes thru all)
-     context "given an input letter" do
-       it "return true if the input letter is in the letter_list" do
-         puzzle = Puzzle.new
-         puzzle.receive_input() #test_files/test2.txt
+describe ".letters_in_wordlist" do #is this letter in the words in the word list (goes thru all)
+ context "given a list of words" do
+   it "will be one array with unique letters looking for" do
+     puzzle = Puzzle.new
+     puzzle.receive_input() #test_files/test2.txt
+     expect(puzzle.letters_in_wordlist()).to eq(["j", "u", "v", "e", "n", "i", "l", "c", "t", "a", "r", "g", "o", "d"])
+     #right now this is failing because input for this function is based off
+     #of the ARGV[0] from receive_input.
+   end
+ end
+end
 
-         expect(puzzle.isin_Words('e')) == (TRUE)
-         expect(puzzle.isin_Words('y')) == (FALSE)
-       end
+describe ".isin_Words" do #is this letter in the words in the word list (goes thru all)
+   context "given an input letter" do
+     it "return true if the input letter is in the letter_list" do
+       puzzle = Puzzle.new
+       puzzle.receive_input() #test_files/test2.txt
+
+       expect(puzzle.isin_Words('e')) == (TRUE)
+       expect(puzzle.isin_Words('y')) == (FALSE)
      end
    end
+ end
 
-   describe ".isWord_bigger_than_puzzle" do
-     context "given a square_with_letters and a word list" do
-       it "return true if length and width are equal" do
-         square_with_letters.size = []
-         expect(variable).to be_in([true, false])
-       end
-     end
-   end
 
+describe ".clean_Square" do #
+    context "given a square_with_letters" do
+      it "will remove tabs, commas, empty lines, returning an array" do
+        puzzle = Puzzle.new
+        puzzle.receive_input() #test_files/test2.txt
+        expect(puzzle.cleanSquare).to include("RATCENTJ")
+      end
+    end
+  end
 
 
 
